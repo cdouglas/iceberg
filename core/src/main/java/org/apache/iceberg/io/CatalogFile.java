252c6e5aa7a2756e515f5a8c2c802d91f683ca95
Chris Douglas <chris_douglas@berkeley.edu>
2024-03-26 22:14:43 +0000

    Remove partial impl of Iceberg serde

--- core/src/main/java/org/apache/iceberg/io/CatalogFile.java
+++ core/src/main/java/org/apache/iceberg/io/CatalogFile.java
@@ -18,75 +18,33 @@
  */
 package org.apache.iceberg.io;
 
-import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.io.Serializable;
 import java.io.UncheckedIOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import org.apache.avro.generic.IndexedRecord;
-import org.apache.iceberg.Schema;
-import org.apache.iceberg.StructLike;
-import org.apache.iceberg.avro.AvroSchemaUtil;
 import org.apache.iceberg.catalog.Namespace;
 import org.apache.iceberg.catalog.TableIdentifier;
-import org.apache.iceberg.types.Types;
 
-public class CatalogFile implements StructLike, IndexedRecord, Serializable {
+public class CatalogFile {
   // TODO use serialization idioms from the project
 
-  // TODO tombstone table data? Or allow ABA?
-  static Types.StructType TABLE =
-      Types.StructType.of(
-          Types.NestedField.required(
-              100,
-              "table",
-              Types.StructType.of(
-                  Types.NestedField.required(102, "namespace", Types.StringType.get()),
-                  Types.NestedField.required(103, "tableName", Types.StringType.get()),
-                  Types.NestedField.optional(104, "location", Types.StringType.get()),
-                  Types.NestedField.optional(105, "metadata", Types.StringType.get()))));
-  static Types.NestedField TABLES =
-      Types.NestedField.optional(
-          106, "tables", Types.ListType.ofRequired(107, TABLE), "list of tables");
-  static Schema SCHEMA = new Schema(TABLES);
-
-  private List<StructLike> tables; // list of tables
   private final Map<TableIdentifier, String> fqti; // fully qualified table identifiers
-
-  public CatalogFile(Map<TableIdentifier, String> fqti) {
-    this.fqti = fqti;
-  }
 
   public CatalogFile() {
     this(new HashMap<>());
   }
 
-  public CatalogFile(List<StructLike> tables) {
-    this.tables = tables;
-    this.fqti = null;
+  public CatalogFile(Map<TableIdentifier, String> fqti) {
+    this.fqti = fqti;
   }
 
   public Map<TableIdentifier, String> fqti() {
     // TODO shouldn't expose internal state like this
     return fqti;
-  }
-
-  public List<StructLike> tables() {
-    return tables;
-  }
-
-  static Schema schema() {
-    return SCHEMA;
   }
 
   public int write(OutputStream out) {
@@ -144,180 +102,4 @@ public class CatalogFile implements StructLike, IndexedRecord, Serializable {
     return fqti.hashCode();
   }
 
-  static class SimpleWriter {
-    // Write the class just using java's POJO serialization
-    public void write(CatalogFile catalogFile, OutputFile outputFile)
-        throws IOException, FileNotFoundException {
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      ObjectOutputStream out = new ObjectOutputStream(baos);
-      try {
-        out.writeObject(catalogFile);
-        PositionOutputStream posOut = outputFile.createOrOverwrite();
-        posOut.write(baos.toByteArray());
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      } finally {
-        out.close();
-      }
-    }
-  }
-
-  static class SimpleReader {
-    // Read the class just using java's POJO serialization
-    public CatalogFile read(InputFile inputFile) throws IOException, FileNotFoundException {
-      SeekableInputStream fis = inputFile.newStream();
-      ObjectInputStream in = new ObjectInputStream(fis);
-      try {
-        return (CatalogFile) in.readObject();
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      } catch (ClassNotFoundException e) {
-        throw new RuntimeException(e);
-      } finally {
-        in.close();
-      }
-    }
-  }
-
-  public static class TableStruct implements StructLike, IndexedRecord, Serializable {
-    private String namespace;
-    private String tableName;
-    private String location;
-    private String metadata; // This is optional based on your schema
-
-    // Constructor
-    public TableStruct(String namespace, String tableName, String location, String metadata) {
-      this.namespace = namespace;
-      this.tableName = tableName;
-      this.location = location;
-      this.metadata = metadata;
-    }
-
-    // Default constructor
-    public TableStruct() {}
-
-    // Implement the StructLike methods
-    @Override
-    public int size() {
-      return 4; // Corresponding to the number of fields: namespace, tableName, location, and
-      // metadata
-    }
-
-    @Override
-    public Object get(int i) {
-      int pos = i;
-      switch (pos) {
-        case 0:
-          return namespace;
-        case 1:
-          return tableName;
-        case 2:
-          return location;
-        case 3:
-          return metadata;
-        default:
-          throw new IllegalArgumentException("Invalid position: " + pos);
-      }
-    }
-
-    @Override
-    public <T> T get(int pos, Class<T> javaClass) {
-      Object value = get(pos);
-      if (value == null) {
-        return null;
-      }
-      return javaClass.cast(value);
-    }
-
-    @Override
-    public void set(int pos, Object value) {
-      switch (pos) {
-        case 0:
-          this.namespace = (String) value.toString();
-          break;
-        case 1:
-          this.tableName = (String) value.toString();
-          break;
-        case 2:
-          this.location = (String) value.toString();
-          break;
-        case 3:
-          this.metadata = (String) value.toString();
-          break;
-        default:
-          throw new IllegalArgumentException("Invalid position: " + pos);
-      }
-      return; // Return the current instance for method chaining
-    }
-
-    // Getters and setters for the fields (if necessary for your application logic)
-
-    @Override
-    public int hashCode() {
-      return Objects.hash(namespace, tableName, location, metadata);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (this == obj) {
-        return true;
-      }
-      if (obj == null || getClass() != obj.getClass()) {
-        return false;
-      }
-      TableStruct other = (TableStruct) obj;
-      return Objects.equals(namespace, other.namespace)
-          && Objects.equals(tableName, other.tableName)
-          && Objects.equals(location, other.location)
-          && Objects.equals(metadata, other.metadata);
-    }
-
-    @Override
-    public org.apache.avro.Schema getSchema() {
-      return AvroSchemaUtil.convert(TABLE, "table");
-    }
-
-    @Override
-    public void put(int i, Object v) {
-      set(i, v);
-    }
-  }
-
-  @Override
-  public org.apache.avro.Schema getSchema() {
-    return AvroSchemaUtil.convert(SCHEMA, "tables");
-  }
-
-  @Override
-  public Object get(int i) {
-    // Since the schema only defines one top-level field ("tables"), we only need to handle that one
-    // case
-    if (i != 0) {
-      throw new IndexOutOfBoundsException("CatalogFile has only one field.");
-    }
-    return tables; // The only field this example class handles
-  }
-
-  @Override
-  public int size() {
-    return SCHEMA.columns().size();
-  }
-
-  @Override
-  public void put(int i, Object v) {
-    set(i, v);
-  }
-
-  @Override
-  public <T> T get(int pos, Class<T> javaClass) {
-    return javaClass.cast(get(pos));
-  }
-
-  @Override
-  public <T> void set(int pos, T value) {
-    switch (pos) {
-      default:
-        put(pos, value);
-    }
-  }
 }
--- core/src/test/java/org/apache/iceberg/io/TestCatalogFile.java
+++ core/src/test/java/org/apache/iceberg/io/TestCatalogFile.java
@@ -20,33 +20,15 @@ package org.apache.iceberg.io;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.apache.iceberg.catalog.TableIdentifier;
-import org.apache.iceberg.inmemory.InMemoryOutputFile;
-import org.apache.iceberg.io.CatalogFile.TableStruct;
 import org.junit.Test;
 
 public class TestCatalogFile {
-  // Test our ability to write and read a CatalogFile
-  @Test
-  public void testCatalogFile() throws Exception {
-    TableStruct table = new TableStruct("ns", "name", "loc", "meta");
-    TableStruct table2 = new TableStruct("ns2", "name2", "loc2", "meta2");
-    CatalogFile catalogFile = new CatalogFile(Arrays.asList(table, table2));
-    CatalogFile.SimpleWriter writer = new CatalogFile.SimpleWriter();
-    InMemoryOutputFile out = new InMemoryOutputFile("catalog.avro");
-    writer.write(catalogFile, out);
-    CatalogFile.SimpleReader reader = new CatalogFile.SimpleReader();
-    CatalogFile readCatalogFile = reader.read(out.toInputFile());
-    for (int i = 0; i < catalogFile.tables().size(); i++) {
-      assert catalogFile.tables().get(i).equals(readCatalogFile.tables().get(i));
-    }
-  }
 
   @Test
-  public void testCatalogFileDirectSerialization() throws Exception {
+  public void testCatalogFileDirectSerialization() {
     CatalogFile catalogFile =
         new CatalogFile(
             Stream.of(
